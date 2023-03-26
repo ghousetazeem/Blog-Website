4caba1cfee1f764c4327a33f2467e86611640d8e
Mohd Ghouse Tazeem <74200389+MohdGhouse-75@users.noreply.github.com>
2023-03-26 13:26:49 +0530

    Updating the functionality

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 //creating constants
-const homeStartingContent = "lorem dfa hh oifjjfaijf sdjfha jsdj  ajdf asdk fjoiajhkf hadsh gksf ag  hfhdjk vdagfggsdhf ha ghak h kjhaipgh  v a f h hfkj fuha hgfa hff dfkj hfh kah fh adfasf lorem dfa hh oifjjfaijf sdjfha jsdj  ajdf asdk fjoiajhkf hadsh gksf ag  hfhdjk vdagfggsdhf ha ghak h kjhaipgh  v a f h hfkj fuha hgfa hff dfkj hfh kah fh adfasf";
-const aboutContent = "haeljaj  jjf a nhan  hjajf lask hegdkajsf djdh gaf hkg hajf hads dk;gjhakdh gkdfhkg hakjh ghfdakjg hadkj ak f ads hfka gak  kadshlf hadsl gug jsd fgkajhflahf";
-const contactContent = "Heheheh skj  hfasd hfahf hkjsdjkf ka h skfkahkjh hfhsddkj ghah fkhau hkjadjdh f dsh kjahf hkdsfhfh  sd hkdh hf jha  dah fhahg jkd h gukf sdhfdsdhfla";
+const homeStartingContent = "Welcome to our blog website! We are a community of writers and thinkers who share a passion for exploring ideas, sharing stories, and engaging in thoughtful discussions. Our blog covers a wide range of topics, from current events and politics to personal growth and wellness.";
+const aboutContent = "Welcome to our blog website! Our about page is designed to help you get to know us and our mission a little better.Our blog is a community of writers and thinkers who are passionate about exploring ideas, sharing stories, and engaging in thoughtful discussions. We believe that everyone has a unique perspective to offer, and we're committed to creating a space where diverse voices can be heard. \n Our team is made up of individuals from a wide range of backgrounds, experiences, and expertise. We are writers, researchers, educators, and professionals who share a common goal: to create a better world through the power of words.We are dedicated to providing our readers with high-quality, informative, and engaging content on a variety of topics, including current events, politics, culture, personal growth, and wellness. \n Our writers bring a fresh and unique perspective to each topic they cover, and we're always on the lookout for new voices to join our community.At our blog, we believe that words have the power to change the world. \n By sharing our thoughts and experiences, we hope to inspire others to think critically, engage in meaningful conversations, and take action to create positive change in their own lives and communities.Thank you for visiting our blog, and we look forward to sharing our journey with you";
+const contactContent = "We would love to hear from you! Our contact page is the best way to get in touch with us, whether you have a question, comment, or just want to say hello.You can reach us by filling out the contact form on this page, or by sending us an email at [insert email address here]. We read every message that comes our way, and we'll do our best to respond as quickly as possible.We also invite you to connect with us on social media. You can follow us on [insert social media platform(s) here] to stay up-to-date on our latest blog posts, news, and events. We love hearing from our readers, and we're always looking for new ways to engage with our community.If you're interested in contributing to our blog, we'd love to hear from you too! We welcome guest posts from writers and thinkers who share our passion for exploring ideas, sharing stories, and engaging in thoughtful discussions. Please visit our Write for Us page for more information on how to submit a guest post.Thank you for taking the time to visit our blog, and we look forward to hearing from you soon!";
 
 //creating global varibles
 let posts = [];
@@ -48,7 +49,7 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
     // const contentTitle = req.body.postTitle;
     // const contentPost = req.body.postContent;
-    // console.log(contentTitle);
+    // console.log(contentTitle);Hellf ajdkfljlajjlkd jfjd 
     // console.log(contentPost);
     // console.log(post["postTitle"]);
     // console.log(post["postContent"]);
@@ -63,17 +64,15 @@ app.post("/compose", function (req, res) {
 
 app.get("/posts/:testing", function(req, res){
     const test = (req.params.testing);
+    const testLower = lodash.lowerCase(test);
     posts.forEach(function(post){
         const title = (post.postTitle);
-        if(title == test){
-            console.log("Match Found!");
-        }
-        else{
-            console.log("Match Not found");
+        const titleLower = lodash.lowerCase(title);
+        if(titleLower === testLower){
+            res.render("post", {postTit: post.postTitle, postCont: post.postContent});
         }
     });
 });
-
 
 
 
